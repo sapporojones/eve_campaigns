bd11375739d5302852597546a0cdfc0829ae31de
Sapporo Jones <sapporojones@localhost.localdomain>
2022-10-27 22:17:05 -0700

    edited settings to set to production

--- eve_campaigns/eve_campaigns/settings.py
+++ eve_campaigns/eve_campaigns/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*#*9a^okmpny@&z1pq2x@ame&gt5zj0(3_^zz!v62a&u2t$6^g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
